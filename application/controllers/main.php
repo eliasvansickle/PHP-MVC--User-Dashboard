d76b081f17a5c4db5f00c44384732e03037324e5
eliasvansickle <eliasvansickle@gmail.com>
2015-06-29 10:26:25 -0700

    First Master commit

---
+++ application/controllers/main.php
@@ -0,0 +1,20 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->output->enable_profiler();
+	}
+
+	public function index()
+	{
+		////Load view file here////
+		$this->load->view('home');
+	}
+}
+
+//end of main controller
+
---
+++ application/views/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset = "utf-8"/>
+		<title>Home</title>
+		<meta name = "description" content="DESCRIPTIONHERE"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<!-- link to custom local css -->
+		<link rel="stylesheet" type="text/css" href="/assets/style_home.css">
+		<!-- link to remote css bootstrap library -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<!-- link to remote js bootstrap library -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<!-- Bootstrap Theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+		<!-- link to remote google jquery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- link to jQuery UI remote css -->
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+		<!-- link to remote google jQuery UI library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+		<!-- link to custom local jquery -->
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
+
